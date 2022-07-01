5c0aad426082e91d3682336f0171f567b48f3d6f
xuxuan <1140865260@qq.com>
2022-07-01 11:06:46 +0800

    project init

---
+++ src/main/java/com/tdlbs/jenkinsboot/JenkinsBootApplication.java
@@ -0,0 +1,13 @@
+package com.tdlbs.jenkinsboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsBootApplication.class, args);
+    }
+
+}
